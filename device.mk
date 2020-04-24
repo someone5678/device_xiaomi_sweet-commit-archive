df02ae857d0f938641f6bb9b184d3a5689499dcd
Roshan Pius <rpius@google.com>
2020-04-24 14:33:28 -0700

    sweet: Add WifiOverlay
    Bug: 148617260
    Test: Device boots up and connects to wifi networks.
    Change-Id: Iec27a6d947a232a42451ef095dc73d1a52327db5

--- device.mk
+++ device.mk
@@ -126,7 +126,8 @@ PRODUCT_PACKAGES += \
     SettingsOverlaySweet \
     SettingsProviderOverlaySweet \
     SystemUIOverlaySweet \
-    TelephonyOverlaySweet
+    TelephonyOverlaySweet \
+    WifiOverlay
 
 # Permissions
 PRODUCT_COPY_FILES += \
