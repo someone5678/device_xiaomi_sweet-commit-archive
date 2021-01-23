a9c0561e663d44369865517f176f4a0a6131ac75
Shubham Das <dasshubham762@gmail.com>
2021-01-23 15:11:31 +0530

    sweet: Add Camera Go
     * Camera Go is way better than Camera2
    
    Change-Id: If5229936dd4a374256c2474eb0da07d6bad3771f

--- device.mk
+++ device.mk
@@ -42,6 +42,16 @@ PRODUCT_PACKAGES += \
     libOmxG711Enc \
     libOmxQcelp13Enc
 
+# Camera
+PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service_64 \
+    libdng_sdk.vendor \
+    libgui_vendor \
+    libxml2 \
+    CameraGo \
+    vendor.qti.hardware.camera.device@1.0.vendor
+
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
@@ -215,7 +225,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
     camera.device@3.6-external-impl \
-    camera.device@3.5-impl
+    camera.device@3.5-impl \
+    libmm-qcamera
 
 PRODUCT_PACKAGES += \
     libshim_megvii
