95f2b57be7e28ae2a4c60ea6919c453d171e1ba6
someone5678 <nemui3353@gmail.com>
2022-03-26 09:39:49 +0900

    Evolution-X bring-ups

--- evolution_sweet.mk
+++ evolution_sweet.mk
@@ -5,18 +5,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common Pixel Experience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common Evolution-X stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 TARGET_INCLUDE_STOCK_ARCORE := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := true
 TARGET_SUPPORTS_QUICK_TAP := true
 
-# Additional Pixel stuffs
-TARGET_INCLUDE_CARRIER_SETTINGS := true
-TARGET_SUPPORTS_APEX_UPDATE := true
+# Additional Evolution-X stuffs
+TARGET_CARRIER_SETTINGS := true
+WITH_GOOGLE_APEX := true
 
-PRODUCT_NAME := aosp_sweet
+PRODUCT_NAME := evolution_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10 Pro
